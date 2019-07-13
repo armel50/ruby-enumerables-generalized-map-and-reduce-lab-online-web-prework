84ff8db619277b938d7c079e58771422d7a78140
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-13 17:02:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,15 +2,11 @@
 def map(array)
   
   yield
+  array
 end
 
-map([1,2,3,-9])do
-  counter = 0
-  while [1,2,3,-9][counter] do
-    [1,2,3,-9][counter] *=  -1
-    counter += 1
-  end
-end
+
+
 
 def reduce(array)
   

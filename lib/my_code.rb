29b3f901d5a4bdf3676cb7feda552878c0358260
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-13 17:22:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,11 +18,9 @@ def reduce(array, starting=0)
   counter = 0 
   sum = starting
   while counter < array.length do 
-    if sum + 1 == false
+    
     sum = yield(array[counter])
-  else
-    sum += yield(array[counter],starting)
-  end
+  
     counter += 1
   
   
